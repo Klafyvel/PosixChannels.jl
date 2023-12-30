d24591f1f53348f8c87ba5fa06a4fe7a1353f7a6
Hugo Levy-Falk <hugo@klafyvel.me>
2023-12-30 22:35:42 +0100

    Fix tests.

--- test/runtests.jl
+++ test/runtests.jl
@@ -5,20 +5,20 @@ using Test
     writer = PosixChannel{Int}("test", mode=:w)
     reader = PosixChannel{Int}("test", mode=:r)
 
-    @test writer.key == reader.key
+    @test writer.name == reader.name
 
     @test length(writer) == 0
-    @test !isavailable(reader)
+    @test !isready(reader)
 
     put!(writer, 1)
 
     @test length(writer) == 1
-    @test isavailable(reader)
+    @test isready(reader)
 
     v = take!(reader)
     @test v == 1
     @test length(reader) == 0
-    @test !isavailable(reader)
+    @test !isready(reader)
 
     close(reader)
     close(writer)
