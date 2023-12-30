836946202784135a91182038d2904cf810242666
Hugo Levy-Falk <hugo@klafyvel.me>
2023-12-30 23:21:11 +0100

    JET tests.

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,9 +1,43 @@
 using PosixChannels
 using Test
 using Aqua
+using JET
 
 @testset "PosixChannels.jl" begin
     Aqua.test_all(PosixChannels)
+
+    @test_call PosixChannel{Int}("jet", mode=:rw)
+    jet = PosixChannel{Int}("jet", mode=:rw)
+    @test_call length(jet)
+    @test_call isnonblocking(jet)
+
+    @test_call put!(jet, 1)
+    @test_call take!(jet)
+    @test_call isready(jet)
+    @test_call wait(jet)
+
+    @test_call close(jet)
+    @test_call unlink(jet)
+
+    @test_call PosixChannels.systemmsgdefault()
+    @test_call PosixChannels.systemmsgmax()
+    @test_call PosixChannels.systemmsgsizedefault()
+    @test_call PosixChannels.systemmsgsizemax()
+    @test_call PosixChannels.systemqueuesmax()
+
+    @test_opt ignored_modules = (Base,) length(jet)
+    @test_opt ignored_modules = (Base,) isnonblocking(jet)
+
+    @test_opt ignored_modules = (Base,) put!(jet, 1)
+    @test_opt ignored_modules = (Base,) take!(jet)
+    @test_opt ignored_modules = (Base,) isready(jet)
+    @test_opt ignored_modules = (Base,) wait(jet)
+
+    @test_opt ignored_modules = (Base,) close(jet)
+    @test_opt ignored_modules = (Base,) unlink(jet)
+
+    close(jet)
+    unlink(jet)
 
     writer = PosixChannel{Int}("test", mode=:w)
     reader = PosixChannel{Int}("test", mode=:r)
